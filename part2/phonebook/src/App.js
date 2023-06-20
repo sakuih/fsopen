994b5ad5e51afe791e9b767ca9967335fb17aa65
taco <taco@taco.taco>
2023-06-20 14:53:27 +0300

    mapping added, deleting under work

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,11 +1,45 @@
-import './App.css';
+import { useState } from 'react'
 
-function App() {
-  return (
-    <div className="App">
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'not connected to db' , number: '3213213'},
+    { name: 'not connected to db' , number: '3213213'},
+    { name: 'not connected to db' , number: '3213213'}
+  ])
+  const [newName, setNewName] = useState('')
 
-    </div>
-  );
-}
+  function deletePerson(e) {
+     console.log("This person will be deleted", e)
+  }
+  function mapPersons(persons) {
+
+      const mappedList = persons.map((person) =>
+          <li key={person.index}>{person.name} {person.number} <button onClick={() => deletePerson(person.index)}>Delete</button></li>
+      )
+    return mappedList
+    }
+
+    return (
+        <div>
+          <h2>Phonebook</h2>
+            Filter <input />
+          <form>
+              <h2>Add a new contact</h2>
+            <div>
+              name: <input /><br/>
+              number: <input />
+            </div>
+            <div>
+              <button type="submit">add</button>
+            </div>
+          </form>
+          <h2>Numbers</h2>
+            <ul>
+                {mapPersons(persons)}
+            </ul>
+
+        </div>
+    )
+  }
 
 export default App;
